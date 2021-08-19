d3849a6a266e719ceedf7cfd4cb0a78fdb7bf885
superkleber <alejandrofaesta@gmail.com>
2021-08-19 15:55:08 -0400

    array in array?

--- studio/schemas/biografia.js
+++ studio/schemas/biografia.js
@@ -1,40 +1,35 @@
 export default {
-    name: 'biografy',
-    title: 'Biografy',
-    type: 'document',
-    fields: [{
-            name: 'fotos',
-            type: 'array',
-            title: "fotos",
-            of: [{
-                    name: 'src',
-                    type: 'url',
-                    title: 'Src',
-                },
-                {
-                    name: 'title',
-                    type: 'string',
-                    title: 'Title',
-                },
-                {
-                    name: 'description',
-                    type: 'string',
-                    title: 'Description',
-                }
-            ]
+  name: "biografy",
+  title: "Biografía",
+  type: "document",
+  fields: [
+    {
+      name: "fotos",
+      type: "array",
+      title: "fotos",
+      of: [
+        {
+          name: "src",
+          type: "url",
+          title: "Src",
         },
         {
-            name: 'epocas',
-            type: 'array',
-            title: "Epocas",
-            of: [{
-                    name: 'year',
-                    type: 'date',
-                    title: 'Year',
-                },
-
-
-            ]
-        }
-    ]
-}
+          name: "title",
+          type: "string",
+          title: "Title",
+        },
+        {
+          name: "description",
+          type: "string",
+          title: "Description",
+        },
+      ],
+    },
+    {
+      name: "epocas",
+      type: "array",
+      title: "Epocas",
+      of: [{ type: "epoca" }],
+    },
+  ],
+};
--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -1,26 +1,30 @@
 // First, we must import the schema creator
-import createSchema from 'part:@sanity/base/schema-creator'
+import createSchema from "part:@sanity/base/schema-creator";
 
 // Then import schema types from any plugins that might expose them
-import schemaTypes from 'all:part:@sanity/base/schema-type'
-import siteConfig from './siteConfig'
-import canciones from './canciones'
-import historias from './historias'
-import biografia from './biografia'
+import schemaTypes from "all:part:@sanity/base/schema-type";
+import siteConfig from "./siteConfig";
+import canciones from "./canciones";
+import historias from "./historias";
+import biografia from "./biografia";
+import evento from "./objects/evento";
+import epoca from "./objects/epoca";
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
-    // We name our schema
-    name: 'default',
-    // Then proceed to concatenate our document type
-    // to the ones provided by any plugins that are installed
-    types: schemaTypes.concat([
-        /* Your types here! */
-        canciones,
-        siteConfig,
-        historias,
-        biografia
+  // We name our schema
+  name: "default",
+  // Then proceed to concatenate our document type
+  // to the ones provided by any plugins that are installed
+  types: schemaTypes.concat([
+    /* Your types here! */
+    canciones,
+    siteConfig,
+    historias,
+    biografia,
 
-
-    ])
-})
+    // Objects
+    epoca,
+    evento,
+  ]),
+});
